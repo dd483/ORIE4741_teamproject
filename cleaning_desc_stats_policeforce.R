5142a92dacc4b4541acfba8bffa445b9785443d8
dd483 <51165999+dd483@users.noreply.github.com>
2020-10-25 23:04:26 -0400

    Add files via upload

--- cleaning_desc_stats_policeforce.R
+++ cleaning_desc_stats_policeforce.R
@@ -49,3 +49,78 @@ hist(df$EventAge)
 
 barplot(prop.table(table(df$ForceType)), main="Force Type Proportion",
         ylim=c(0,1),cex.names=0.6)
+
+write.csv(df,"Police_Use_of_Force_cleaned.csv")
+
+
+#RACE AND FORCE TYPE
+data = df %>%
+  group_by(ForceType, Race) %>%
+  summarise(n = n()) %>% 
+  mutate(freq = n / sum(n))
+
+ggplot(data, aes(x = Race, y = freq))+
+  geom_bar(
+    aes(fill = ForceType), stat = "identity", color = "white",
+    position = position_dodge(0.9)
+  ) +  
+  xlab("Race") + 
+  ylab("Proportional Frequency") +
+  ggtitle("Force Type Outcome in Overall Proportions by Race") 
+
+data = df %>%
+  group_by(ForceType, Race) %>%
+  summarise(n = n()) 
+data = spread(data, ForceType, n)
+for (i in 1:12) {
+  for (j in 1:7) {
+    if (is.na(data[j,i])) {
+      data[j,i] = 0  
+    }    
+  }
+}
+data$n = data$Baton + data$`Bodily Force` + data$`Chemical Irritant`+
+  data$Firearm + data$`Gun Point Display` + data$`Improvised Weapon`+
+  data$`Less Lethal` + data$`Less Lethal Projectile`+ data$`Maximal Restraint Technique`+
+  data$`Police K9 Bite` + data$Taser
+for (i in 2:12) {
+  for (j in 1:7) {
+    data[j,i] = data[j,i]/data[j,13]  
+  }
+}
+data = data[,1:12]
+
+data_long = gather(data, ForceType, 
+                   prop, Baton:`Bodily Force`:`Chemical Irritant`:Firearm:`Gun Point Display`:
+                     `Improvised Weapon`:`Less Lethal`:`Less Lethal Projectile`:`Maximal Restraint Technique`:
+                     `Police K9 Bite`:Taser,
+                   factor_key=TRUE)
+ggplot(data_long, aes(x = Race, y = prop))+
+  geom_bar(
+    aes(fill = ForceType), stat = "identity", color = "white",
+    position = position_dodge(0.9)
+  ) +  
+  xlab("Race") + 
+  ylab("Proportional Frequency") +
+  ggtitle("Citation Outcome in Race Proportions") 
+
+
+
+#Precinct
+data = df[df$Precinct != "0",]
+data = data[data$Precinct != "0M",]
+data = data[data$Precinct != "0O",]
+data = data[data$Precinct != "0U",]
+data = data %>%
+  group_by(ForceType, Precinct) %>%
+  summarise(n = n()) %>% 
+  mutate(freq = n / sum(n))
+
+ggplot(data, aes(x = Precinct, y = freq))+
+  geom_bar(
+    aes(fill = ForceType), stat = "identity", color = "white",
+    position = position_dodge(0.9)
+  ) +  
+  xlab("Precinct") + 
+  ylab("Proportional Frequency") +
+  ggtitle("Force Type Outcome in Overall Proportions by Precinct")
