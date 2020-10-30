c79b6620446e9fb48f5bab7af481c5704d903678
dd483 <51165999+dd483@users.noreply.github.com>
2020-10-29 22:04:31 -0400

    Add files via upload

--- cleaning_desc_stats_policeforce.R
+++ cleaning_desc_stats_policeforce.R
@@ -1,10 +1,15 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(data.table)
+library(mltools)
+library(stringr)
 
 df = read.csv("Police_Use_of_Force_nonmissing.csv")
 dim(df)
 
+##CLEANING DATA
+#check missing values
 colnames(df)
 summary(is.na(df$Problem))
 summary(is.na(df$Is911Call))
@@ -19,24 +24,55 @@ summary(is.na(df$TypeofResistance))
 summary(is.na(df$Precinct))
 summary(is.na(df$Neighborhood))
 
-
+#trim and view proportions
 table(df$Problem)
-table(df$Is911Call)
-table(df$PrimaryOffense)
-table(df$SubjectInjury)
-table(df$SubjectRole)
-table(df$SubjectRoleNumber)
-table(df$ForceType)
-table(df$ForceTypeAction)
-table(df$Race)
-table(df$Sex)
-table(df$TypeOfResistance)
-table(df$Precinct)
-table(df$Neighborhood)
+df$Problem = str_trim(df$Problem)
 
+table(df$Is911Call)
+df$Is911Call = str_trim(df$Is911Call)
+
+table(df$PrimaryOffense)
+df$PrimaryOffense = str_trim(df$PrimaryOffense)
+
+table(df$SubjectInjury)
+
+table(df$ForceReportNumber)
+
+table(df$SubjectRole)
+df$SubjectRole = str_trim(df$SubjectRole)
+
+table(df$SubjectRoleNumber)
+
+table(df$ForceType)
+df$ForceType = str_trim(df$ForceType)
+
+table(df$ForceTypeAction)
+df$ForceTypeAction = str_trim(df$ForceTypeAction)
+
+table(df$Race)
+df$Race = str_trim(df$Race)
+
+table(df$Sex)
+
+table(df$TypeOfResistance)
+df$TypeofResistance = str_trim(df$TypeofResistance)
+
+table(df$Precinct)
+
+table(df$Neighborhood)
+df$Neighborhood = str_trim(df$Neighborhood)
+
+#drop erroneous precincts
+df = df[df$Precinct != "0",]
+df = df[df$Precinct != "0M",]
+df = df[df$Precinct != "0O",]
+df = df[df$Precinct != "0U",]
+
+#check latitudes
 summary(df$CenterLatitude)
 
 df = df[df$Race != "not recorded",]
+df = df[df$Sex != "not recorded",] #drops 5
 df = df[df$CenterLatitude != 0,]
 dim(df)
 
@@ -50,9 +86,116 @@ hist(df$EventAge)
 barplot(prop.table(table(df$ForceType)), main="Force Type Proportion",
         ylim=c(0,1),cex.names=0.6)
 
+
+#standardize string entries
+df$TypeOfResistance = gsub(" ", "", df$TypeOfResistance, fixed = TRUE) 
+df$TypeOfResistance[df$TypeOfResistance == "AssaultedOfficer" | 
+                      df$TypeOfResistance == "AssaultedPoliceK9" |
+                      df$TypeOfResistance == "AssaultingPoliceK9"] = "Assault Officer"
+df$TypeOfResistance[df$TypeOfResistance == "AssaultingPoliceHorse" |
+                      df$TypeOfResistance == "AssaultedPoliceHorse"] = "Assault Police Horse"
+df$TypeOfResistance[df$TypeOfResistance == "CommissionofaCrime" | 
+                      df$TypeOfResistance == "commissionofcrime" |
+                      df$TypeOfResistance == "CommissionofCrime" |
+                      df$TypeOfResistance == "COMMISSIONOFCRIME"] = "Commission of Crime"
+df$TypeOfResistance[df$TypeOfResistance == "FledinaVehicle" |
+                      df$TypeOfResistance == "FledinVehicle"] = "Fled in Vehicle"
+df$TypeOfResistance[df$TypeOfResistance == "FledonFoot"] = "Fled on Foot"
+df$TypeOfResistance[df$TypeOfResistance == "tensed" |
+                      df$TypeOfResistance == "Tensed" |
+                      df$TypeOfResistance == "TENSED"] = "Tensed"
+df$TypeOfResistance[df$TypeOfResistance == "verbalnon-compliance" |
+                      df$TypeOfResistance == "VerbalNon-Compliance"] = "Verbal Non-Compliance"
+
+df$ForceTypeAction[df$ForceTypeAction == "Body Weight Pin"] = "Body Weight to Pin"
+df$ForceTypeAction[df$ForceTypeAction == "Knee"] = "Knees"
+df$ForceTypeAction[df$ForceTypeAction == "Punch"] = "Punches"
+df$ForceTypeAction[df$ForceTypeAction == "Slap"] = "Slaps"
+
+
+df$ResponseDate_year=as.numeric(substr(df$ResponseDate, 1,4))
+df$ResponseDate_month=as.numeric(substr(df$ResponseDate,6,7))
+df$ResponseDate_day=as.numeric(substr(df$ResponseDate,9,10))
+df$ResponseDate_hour=as.numeric(substr(df$ResponseDate,12,13))
+df$ResponseDate_minute=as.numeric(substr(df$ResponseDate,15,16))
+
 write.csv(df,"Police_Use_of_Force_cleaned.csv")
 
 
+#PREPROCESSING
+df = read.csv("Police_Use_of_Force_cleaned.csv")
+df$Neighborhood_m = "Other"
+df$Neighborhood_m[df$Neighborhood == "Downtown West"] = "Downtown West"
+df$Neighborhood_m[df$Neighborhood == "Jordan"] = "Jordan"
+df$Neighborhood_m[df$Neighborhood == "Hawthorne"] = "Hawthorne"
+df$Neighborhood_m[df$Neighborhood == "Near - North"] = "Near - North"
+df$Neighborhood_m[df$Neighborhood == "Willard - Hay"] = "Willard - Hay"
+
+df$SubjectRole_m = "Other"
+df$SubjectRole_m[df$SubjectRole == "A"] = "A"
+df$SubjectRole_m[df$SubjectRole == "ARRESTEE"] = "ARRESTEE"
+df$SubjectRole_m[df$SubjectRole == "S"] = "S"
+df$SubjectRole_m[df$SubjectRole == "OT"] = "OT"
+df$SubjectRole_m[df$SubjectRole == "SUSPECT"] = "SUSPECT"
+
+df$PrimaryOffense_m = "Other"
+df$PrimaryOffense_m[df$PrimaryOffense == "OBSTRU"] = "OBSTRU"
+df$PrimaryOffense_m[df$PrimaryOffense == "DISCON"] = "DISCON"
+df$PrimaryOffense_m[df$PrimaryOffense == "ASLT4"] = "ASLT4"
+df$PrimaryOffense_m[df$PrimaryOffense == "FORCE"] = "FORCE"
+df$PrimaryOffense_m[df$PrimaryOffense == "NARC"] = "NARC"
+
+df$Problem_m = "Other"
+df$Problem_m[df$Problem == "Suspicious Person"] = "Suspicious Person"
+df$Problem_m[df$Problem == "Fight"] = "Fight"
+df$Problem_m[df$Problem == "Disturbance"] = "Disturbance"
+df$Problem_m[df$Problem == "Domestic Abuse-In Progress"] = "Domestic Abuse-In Progress"
+df$Problem_m[df$Problem == "Suspicious Vehicle"] = "Suspicious Vehicle"
+df$Problem_m[df$Problem == "Traffic Law Enforcement"] = "Traffic Law Enforcement"
+df$Problem_m[df$Problem == "Unknown Trouble"] = "Unknown Trouble"
+df$Problem_m[df$Problem == "Assault in Progress"] = "Assault in Progress"
+df$Problem_m[df$Problem == "Unwanted Person"] = "Unwanted Person"
+df$Problem_m[df$Problem == "Emotionally Disturb Person"] = "Emotionally Disturb Person"
+
+df$ForceTypeAction_m = "Other"
+df$ForceTypeAction_m[df$ForceTypeAction == "Body Weight to Pin"] = "Body Weight to Pin"
+df$ForceTypeAction_m[df$ForceTypeAction == "Punches"] = "Punches"
+df$ForceTypeAction_m[df$ForceTypeAction == "Joint Lock"] = "Joint Lock"
+df$ForceTypeAction_m[df$ForceTypeAction == "Personal Mace"] = "Personal Mace"
+df$ForceTypeAction_m[df$ForceTypeAction == "Push Away"] = "Push Away"
+df$ForceTypeAction_m[df$ForceTypeAction == "Knees"] = "Knees"
+df$ForceTypeAction_m[df$ForceTypeAction == "Crowd Control Mace"] = "Crowd Control Mace"
+df$ForceTypeAction_m[df$ForceTypeAction == "Firing Darts"] = "Firing Darts"
+df$ForceTypeAction_m[df$ForceTypeAction == "Kicks"] = "Kicks"
+df$ForceTypeAction_m[df$ForceTypeAction == "Takedown"] = "Takedown"
+
+
+write.csv(df,"Police_Use_of_Force_preprocessed.csv")
+
+
+
+##ONE HOT
+df = read.csv("Police_Use_of_Force_preprocessed.csv")
+
+df$Precinct = as.factor(df$Precinct)
+data = df
+reqd = c("Problem_m", "Is911Call", "PrimaryOffense_m","SubjectInjury","SubjectRole_m",
+          "ForceTypeAction_m","Race","Sex","TypeOfResistance","Precinct","Neighborhood_m")
+data = data[,reqd]
+newdata = one_hot(as.data.table(data))
+newdata[is.na(newdata)] = 0
+data = cbind(df,newdata)
+drop = c("X.3", "X.2", "X.1","Problem", "Is911Call", "PrimaryOffense","SubjectInjury","SubjectRole",
+         "ForceTypeAction","Race","Sex","TypeOfResistance","Precinct","Neighborhood",
+         "Neighborhood_m","SubjectRole_m","PrimaryOffense_m","Problem_m","ForceTypeAction_m")
+data = data[ , !(names(data) %in% drop)]
+df = data
+write.csv(data,"Police_Use_of_Force_onehot.csv")
+
+
+
+
+##VISUALIZATIONS
 #RACE AND FORCE TYPE
 data = df %>%
   group_by(ForceType, Race) %>%
@@ -107,11 +250,7 @@ ggplot(data_long, aes(x = Race, y = prop))+
 
 
 #Precinct
-data = df[df$Precinct != "0",]
-data = data[data$Precinct != "0M",]
-data = data[data$Precinct != "0O",]
-data = data[data$Precinct != "0U",]
-data = data %>%
+data = df %>%
   group_by(ForceType, Precinct) %>%
   summarise(n = n()) %>% 
   mutate(freq = n / sum(n))
@@ -124,3 +263,4 @@ ggplot(data, aes(x = Precinct, y = freq))+
   xlab("Precinct") + 
   ylab("Proportional Frequency") +
   ggtitle("Force Type Outcome in Overall Proportions by Precinct")
+
--- cleaning_desc_stats_policestop.R
+++ cleaning_desc_stats_policestop.R
@@ -6,9 +6,25 @@ df = read.csv("Police_Stop_Data_nonmissing.csv")
 dim(df)
 colnames(df)
 
+##CLEANING DATA
+#missing values
+summary(is.na(df$reason))
+summary(is.na(df$problem))
+summary(is.na(df$callDisposition))
+summary(is.na(df$personSearch))
+summary(is.na(df$vehicleSearch))
+summary(is.na(df$preRace))
+summary(is.na(df$race))
+summary(is.na(df$gender))
+summary(is.na(df$policePrecinct))
+summary(is.na(df$neighborhood))
+
+
+#trim and view proportions
 table(df$reason)
 table(df$problem)
 table(df$callDisposition)
+df$callDisposition = str_trim(df$callDisposition)
 table(df$citationIssued)
 table(df$personSearch)
 table(df$vehicleSearch)
@@ -17,15 +33,76 @@ table(df$race)
 table(df$gender)
 table(df$policePrecinct)
 table(df$neighborhood)
+df$neighborhood = str_trim(df$neighborhood)
 summary(df$lat)
 
+#drop erroneous precincts
 df = df[df$lat > 0,]
-
+df = df[!is.na(df$policePrecinct),]
 summary(df$lat)
 summary(df$long)
 
+
 barplot(prop.table(table(df$citationIssued)), main="Citation Issued Proportion",
         ylim=c(0,1))
+
+df$responseDate_year=as.numeric(substr(df$responseDate, 1,4))
+df$responseDate_month=as.numeric(substr(df$responseDate,6,7))
+df$responseDate_day=as.numeric(substr(df$responseDate,9,10))
+df$responseDate_hour=as.numeric(substr(df$responseDate,12,13))
+df$responseDate_minute=as.numeric(substr(df$responseDate,15,16))
+
+write.csv(df,"Police_Stop_Data_cleaned.csv")
+
+#PREPROCESSING
+df = read.csv("Police_Stop_Data_cleaned.csv")
+df$callDisposition_m = "Other"
+df$callDisposition_m[df$callDisposition == "ADV-Advised"] = "ADV-Advised"
+df$callDisposition_m[df$callDisposition == "GOA-Gone on Arrival"] = "GOA-Gone on Arrival"
+df$callDisposition_m[df$callDisposition == "TAG-Tagged"] = "TAG-Tagged"
+df$callDisposition_m[df$callDisposition == "AOK- All OK"] = "AOK- All OK"
+df$callDisposition_m[df$callDisposition == "BKG-Booking"] = "BKG-Booking"
+df$callDisposition_m[df$callDisposition == "SNT-Sent"] = "SNT-Sent"
+df$callDisposition_m[df$callDisposition == "RPT-Report"] = "RPT-Report"
+df$callDisposition_m[df$callDisposition == "UTL-Unable to Locate"] = "UTL-Unable to Locate"
+df$callDisposition_m[df$callDisposition == "INF-Information"] = "INF-Information"
+df$callDisposition_m[df$callDisposition == "TRN-Transport"] = "TRN-Transport"
+
+df$neighborhood_m = "Other"
+df$neighborhood_m[df$neighborhood == "Downtown West"] = "Downtown West"
+df$neighborhood_m[df$neighborhood == "Hawthorne"] = "Hawthorne"
+df$neighborhood_m[df$neighborhood == "Near - North"] = "Near - North"
+df$neighborhood_m[df$neighborhood == "Whittier"] = "Whittier"
+df$neighborhood_m[df$neighborhood == "Jordan"] = "Jordan"
+df$neighborhood_m[df$neighborhood == "Marcy Holmes"] = "Marcy Holmes"
+df$neighborhood_m[df$neighborhood == "Lyndale"] = "Lyndale"
+df$neighborhood_m[df$neighborhood == "Folwell"] = "Folwell"
+df$neighborhood_m[df$neighborhood == "Willard - Hay"] = "Willard - Hay"
+df$neighborhood_m[df$neighborhood == "Lowry Hill East"] = "Lowry Hill East"
+
+write.csv(df,"Police_Stop_Data_preprocessed.csv")
+
+##ONE HOT
+df = read.csv("Police_Stop_Data_preprocessed.csv")
+
+df$policePrecinct = as.factor(df$policePrecinct)
+data = df
+reqd = c("reason", "problem", "callDisposition_m","personSearch","vehicleSearch",
+         "preRace","race","gender","policePrecinct","neighborhood_m")
+data = data[,reqd]
+newdata = one_hot(as.data.table(data))
+newdata[is.na(newdata)] = 0
+data = cbind(df,newdata)
+drop = c("X","callDisposition_m","neighborhood_m","reason","problem","personSearch",
+         "vehicleSearch","preRace","race","gender","policePrecinct","callDisposition",
+         "neighborhood")
+data = data[ , !(names(data) %in% drop)]
+df = data
+write.csv(df,"Police_Stop_Data_onehot.csv")
+
+
+
+
 
 #RACE AND CITATION
 data = df %>%
